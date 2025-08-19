7f6211350fdd24ef7b507970a441c0fffbb4dda1
Saed Abukar <saed.a.abukar@gmail.com>
2025-08-19 15:22:30 +0300

    First lesson

--- src/main/kotlin/Week1/Exercise2.kt
+++ src/main/kotlin/Week1/Exercise2.kt
@@ -1,15 +1,31 @@
 package Week1
 
 fun main() {
-    val numbers = mutableListOf(7)
-    for (i in 1..7) {
+    /*val numbers: MutableSet<Int> = mutableSetOf()
+    while (numbers.size < 7) {
         val number = (1..40).random()
-        if (number in numbers) {
-            println("Number is in the list $number")
-            continue
-        }
-        println("Number is not in the list $number")
         numbers.add(number)
     }
-    println(numbers)
+    println(numbers.sorted())*/
+    val nums1: MutableSet<Int> = mutableSetOf(1,2,3,4,5,6,7,8,9,10) // false
+    val nums2: MutableList<Int> = mutableListOf(1,2,3,4,5,6,7) // true
+    val nums3: Set<Int> = setOf(1,2,3,4,5,6) // false
+    val nums4: List<Int> = listOf(1,2,3,4,5,6,41) // false
+    val nums5: MutableSet<Int> = mutableSetOf(0,1,2,3,4,5,6) // false
+    val nums6: Set<Int> = setOf(1,2,3,4,5,6,7) // true
+
+    println(isLegalLottoNumbers(nums1))
+    println(isLegalLottoNumbers(nums2))
+    println(isLegalLottoNumbers(nums3))
+    println(isLegalLottoNumbers(nums4))
+    println(isLegalLottoNumbers(nums5))
+    println(isLegalLottoNumbers(nums6))
+}
+
+fun isLegalLottoNumbers(numbers: Collection<Int>): Boolean {
+    if (numbers.size != 7 && numbers.toSet().size != 7) return false
+    for (number in numbers) {
+        if (number !in 1..40) return false
+    }
+    return true
 }
